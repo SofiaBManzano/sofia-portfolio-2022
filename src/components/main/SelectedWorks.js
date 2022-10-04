71e1bda4098fc5ec4e5e5b0c4cf9b1c630a6a95a
SofiaBManzano <sofiabmanzano@gmail.com>
2022-10-04 11:21:50 +0200

    add: new works

--- src/components/main/SelectedWorks.js
+++ src/components/main/SelectedWorks.js
@@ -10,6 +10,30 @@ const SelectedWorks = () => {
           in React using an external API."
           href="https://sofiabmanzano.github.io/Buscador-Harry-Potter---React/#/"
         />
+        <TemplateWork
+          title="Configurador de ventiladores"
+          description="//Responsive search engine for Harry Potter characters developed
+          in React using an external API."
+          href="https://sofiabmanzano.github.io/Buscador-Harry-Potter---React/#/"
+        />
+        <TemplateWork
+          title="Funcionalidad de filtro por países en página real"
+          description="//Responsive search engine for Harry Potter characters developed
+          in React using an external API."
+          href="https://sofiabmanzano.github.io/Buscador-Harry-Potter---React/#/"
+        />
+        <TemplateWork
+          title="Make it fresh"
+          description="//Responsive search engine for Harry Potter characters developed
+          in React using an external API."
+          href="https://sofiabmanzano.github.io/Buscador-Harry-Potter---React/#/"
+        />
+         <TemplateWork
+          title="Back to school"
+          description="//Responsive search engine for Harry Potter characters developed
+          in React using an external API."
+          href="https://sofiabmanzano.github.io/Buscador-Harry-Potter---React/#/"
+        />
       </ul>
     </section>
   );
