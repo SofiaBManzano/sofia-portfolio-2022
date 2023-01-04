c60ffdaf46c0e11e08fc2c5b922dc7e062471001
Sofia B. Manzano <sofiabmanzano@gmail.com>
2023-01-04 09:13:03 +0100

    wip: menu colapsible && styles

--- src/components/header/ItemsMenu.js
+++ src/components/header/ItemsMenu.js
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 const ItemsMenu = (props) => {
   return (
-    <nav className={props.menuOpen}>
-      <ul>
-        <li>
-          <Link to="/">Ir al inicio</Link>
+    <nav className={`c-header-nav ${props.menuOpen}`}>
+      <ul className="c-header-nav-content">
+        <li className="c-header-nav-item">
+          <Link to="/" className="c-header-nav-item-link">Ir al inicio</Link>
         </li>
-        <li>
-          <Link to="/contacto">Ir a contacto</Link>
+        <li className="c-header-nav-item">
+          <Link to="/contacto" className="c-header-nav-item-link">Ir a contacto</Link>
         </li>
-        <li>
-          <Link to="/about">¿Por qué yo?</Link>
+        <li className="c-header-nav-item">
+          <Link to="/about" className="c-header-nav-item-link">¿Por qué yo?</Link>
         </li>
-        <li>
-          <Link to="/trabajos-seleccionados">Trabajos seleccionados</Link>
+        <li className="c-header-nav-item">
+          <Link to="/trabajos-seleccionados" className="c-header-nav-item-link">Trabajos seleccionados</Link>
         </li>
       </ul>
     </nav>
